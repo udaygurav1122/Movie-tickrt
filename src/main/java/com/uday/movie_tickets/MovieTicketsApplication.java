74e2ca5180224951092ed9e9cac2f73277ad3aa4
uday <udayagurav@gmail.com>
2024-08-03 18:11:00 +0530

    done

---
+++ src/main/java/com/uday/movie_tickets/MovieTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.uday.movie_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uday/movie_tickets/MovieTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uday.movie_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
